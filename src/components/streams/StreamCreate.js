d71181505fff64da9cca6e4ccdbf328e90f1e6ac
thach90 <tkach90@yahoo.com.ua>
2020-02-21 04:55:23 +0200

    add labels to the form and validation function

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,26 +2,61 @@ import React from 'react';
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-    renderInput({ input }) {
-        return <input {...input}/>
+    renderInput({ input, label }) {
+        return (
+            <div className="field">
+                <label>{ label }</label>
+                <input {...input}/>
+            </div>
+        );
+    }
+
+    onSubmit(formValues) {
+        console.log('formValues',formValues);
+
     }
 
     render() {
         return (
-            <form>
+            <form
+                className="ui form"
+                onSubmit={this.props.handleSubmit(this.onSubmit)}
+            >
                 <Field
                     name="title"
                     component={this.renderInput}
+                    label="Enter Title"
                 />
                 <Field
                     name="description"
                     component={this.renderInput}
+                    label="Enter Description"
                 />
+                <button
+                    className="ui button primary"
+                >
+                    Submit
+                </button>
             </form>
         );
     }
 }
 
+const validate = (formValues) => {
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = 'You must enter a title';
+    }
+
+    if (!formValues.description) {
+        errors.description = 'You must enter a description';
+    }
+
+    return errors;
+};
+
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate
 })(StreamCreate);
